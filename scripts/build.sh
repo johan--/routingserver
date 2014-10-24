990f6dbc1987d514920a4419bc451d82d05f515a
David Sachitano <dsachita@gmail.com>
2014-10-23 20:41:15 -0400

    going back to creating a resources dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-zip -j build.zip docker-geoserver/Dockerfile docker-geoserver/Dockerrun.aws.json
+zip -j build.zip docker-geoserver/Dockerfile docker-geoserver/Dockerrun.aws.json 
+zip build.zip resources/
