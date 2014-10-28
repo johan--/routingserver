cd016714323f40c1a41abb57c37bbb58724961d2
David Sachitano <dsachita@gmail.com>
2014-10-28 16:58:50 -0400

    tweaks to zip build and ports

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,10 @@
 zip -j geoserver_build.zip docker-geoserver/Dockerfile docker-geoserver/Dockerrun.aws.json 
 zip geoserver_build.zip resources/
 
-zip -j postgis_build.zip docker-postgis/Dockerfile docker-postgis/Dockerrun.aws.json
+zip -j postgis_build.zip docker-postgis/Dockerfile 
+#zip -j postgis_build.zip docker-postgis/Dockerrun.aws.json 
+zip -j postgis_build.zip docker-postgis/postgres.conf
+zip -j postgis_build.zip docker-postgis/sshd.conf
+zip -j postgis_build.zip docker-postgis/setup.sh
+zip -j postgis_build.zip docker-postgis/start-postgis.sh
+zip -j postgis_build.zip docker-postgis/run-postgis-docker.sh
