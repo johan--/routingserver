f6528f85b244a726692d6e0db3ce64a345637682
David Sachitano <dsachita@gmail.com>
2014-10-28 14:45:37 -0400

    initial attempt at adding a postgis container
    * pulled from the same kartoza people as the geoserver
      Dockerfile
    * just copying the source instead of git submodules
    * trying to configure travis to build two artifacts,
      and then these containers will be launched separately

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-zip -j build.zip docker-geoserver/Dockerfile docker-geoserver/Dockerrun.aws.json 
-zip build.zip resources/
+zip -j geoserver_build.zip docker-geoserver/Dockerfile docker-geoserver/Dockerrun.aws.json 
+zip geoserver_build.zip resources/
+
+zip -j postgis_build.zip docker-postgis/Dockerfile docker-postgis/Dockerrun.aws.json
